d3ae27b5cd1c3c22d5387f5d3494aac17e3c1a5e
Lucas <lukas_boo@hotmail.com>
2021-09-08 13:08:34 -0300

    Initial Commit

---
+++ MoviesMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesMVVM
+//
+//  Created by Lucas Daniel on 08/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
