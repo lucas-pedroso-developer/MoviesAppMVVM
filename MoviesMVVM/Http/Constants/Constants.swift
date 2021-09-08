904fb7b7c654a233f5dc9e26b0abd018eec57c0d
Lucas <lukas_boo@hotmail.com>
2021-09-08 13:53:49 -0300

    creating constants

---
+++ MoviesMVVM/Http/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MoviesMVVM
+//
+//  Created by Lucas Daniel on 08/09/21.
+//
+
+import Foundation
+
+class Constants {
+    static let API_KEY = "JBzDusoqkuiMuax3qUcGrQMZPR0Cc1Vs"
+    static let BASE_URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=&api-key=" + API_KEY
+}
