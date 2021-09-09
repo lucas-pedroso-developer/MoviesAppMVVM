3925a9abbe8f9f41937d1f9563a1892952a003f2
Lucas <lukas_boo@hotmail.com>
2021-09-09 08:21:55 -0300

    creating main protocol

---
+++ MoviesMVVM/Protocol/MainProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MainProtocol.swift
+//  MoviesMVVM
+//
+//  Created by Lucas Daniel on 09/09/21.
+//
+
+import Foundation
+
+public protocol MainProtocol {
+    func fetch()
+}
+    
+
--- MoviesMVVM/ViewModel/MainViewModel.swift
+++ MoviesMVVM/ViewModel/MainViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+public class MainViewModel {
+    
+    var movies: Movies?
+    let http = Http()
+    var delegate: MainProtocol?
+    
+    public init() {}
+    
+    func getmovies(url: String) {
+        http.get(url: url) { result in
+            switch result {
+            case .success(let data):
+                if data != nil {
+                    self.movies = data?.toModel()                    
+                } 
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+}
